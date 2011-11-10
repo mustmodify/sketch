1f9ceaf312d368a271677327dbc3f13a1425fb0e
Johnathon E. Wright <jw@mustmodify.com>
2011-11-10 12:03:20 -0600

    update circle to use svg_attributes

--- lib/sketch/circle.rb
+++ lib/sketch/circle.rb
@@ -4,11 +4,17 @@ class Sketch::Circle < Sketch::Element
   has_value :cy
   has_value :r, :alias => :radius
   has_value :fill
+  has_value :fill_opacity
   has_value :stroke 
   has_value :stroke_width
 
-  def draw(canvas)
-    canvas.circle(:cx => self.cx, :cy => self.cy, :r => self.radius, :fill => self.fill, :stroke => self.stroke, 'stroke_width' => self.stroke_width)
+  def point=(coordinates)
+    self.cx, self.cy = *coordinates
   end
+
+  def point
+    [self.cx, self.cy]
+  end
+
 end
 
